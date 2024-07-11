9481677b49d2b8fbaec5b0773833381be56fff82
ilya_popov <il_pop@mail.ru>
2024-07-11 15:00:07 +0300

    class DeleteUser -> удаление пользователя

--- my_blog/registration/urls.py
+++ my_blog/registration/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('registration/', views.RegisterUser.as_view(), name="registration"),  # регистрация пользователя
     path('login/', views.LoginUser.as_view(), name="login"),  # аутентификация пользователя
     path('logout/', views.LogoutUser.as_view(), name='logout'),  # выход из приложения
-
+    path('delete/<int:pk>', views.DeleteUser.as_view(), name='delete_user'),  # удаление пользователя
 ]
--- my_blog/registration/views.py
+++ my_blog/registration/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 
 from . import forms
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -51,3 +52,12 @@ class LogoutUser(LogoutView):
 
     def get_success_url(self):
         return reverse_lazy("app_blog:index")
+
+
+class DeleteUser(DeleteView):  # TODO сделать Mixin, только для администратора
+    """
+    Удалить пользователя
+    """
+    template_name = "registration/delete_user.html"
+    model = User
+    success_url = reverse_lazy("app_blog:index")
